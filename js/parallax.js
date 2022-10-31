65f23b0fe932f0ab8233c81b451af5027fdae523
Philip Habib <philip.habib6347@gmail.com>
2022-10-31 11:29:25 -0400

    Almost full commit for netlify, no video, not minified

--- js/parallax.js
+++
@@ -1,8 +0,0 @@
-const parallaxImgs = document.querySelectorAll(".parallax");
-
-window.addEventListener("scroll", () => {
-  console.log(window.scrollY);
-  parallaxImgs.forEach((parallaxImg) => {
-    parallaxImg.style.backgroundPositionY = window.scrollY * -0.2 + "px";
-  });
-});
